53c1801f2b90b1c40b76f34f9c811a7eb48dbf4c
kuhaha <kevin.yr.cheng@gmail.com>
2024-08-25 13:05:29 +0900

    Update pg_header.php

--- src/views/pg_header.php
+++ src/views/pg_header.php
@@ -9,15 +9,15 @@
   <body>
   <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom border-bottom-dark">
   <div class="container-fluid ">
-    <a class="navbar-brand" href="?to=usr&do=list">SPL2024</a>
+    <a class="navbar-brand" href="?to=prg&do=list">SPL2024</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="?to=usr&do=list">HOME</a>
+        <a class="nav-link active" aria-current="page" href="?to=prg&do=list">HOME</a>
         <a class="nav-link" href="?to=usr&do=login">LOGIN</a>
-        <a class="nav-link" href="?to=usr&do=list">LIST</a>
+        <a class="nav-link" href="?to=prg&do=list">LIST</a>
         <a class="nav-link disabled">HELP</a>
       </div>
     </div>
