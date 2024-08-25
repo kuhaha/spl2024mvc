3774cf15fd43ca2b066ce83445a777e9fff92c30
kuhaha <kevin.yr.cheng@gmail.com>
2024-08-25 13:29:52 +0900

    Update Router.php

--- src/Router.php
+++ src/Router.php
@@ -5,7 +5,24 @@ class Router
 {
     private $base = null;
     private $routes = ['GET'=>[],'POST'=>[]];
-
+/*
+$base = dirname($_SERVER['PHP_SELF']);
+$routes = [
+   //method, path, [(c)ontroller, (a)ction, [(p)arameters]]
+    ['GET', '/', ['c'=>'User', 'a'=>'list']],
+    ['GET', '/usr/login/?', ['c'=>'User', 'a'=>'login']],
+    ['GET', '/usr/list/?', ['c'=>'User','a'=>'list']],
+    ['GET', '/usr/([0-9]+)/detail', ['c'=>'User','a'=>'detail','p'=>['id']] ],
+    ['GET', '/usr/([0-9]+)/modify', ['c'=>'User','a'=>'modify','p'=>['id']] ],
+    ['GET', '/usr/input', ['c'=>'User','a'=>'input'] ],
+    ['POST', '/usr/save', ['c'=>'User','a'=>'save'] ],
+    ['GET', '/s/list', ['c'=>'User','a'=>'list']],
+    ['GET', '/s/([a-z0-9]+)/detail', ['c'=>'Student','a'=>'detail','p'=>['sid']] ],
+    ['GET', '/s/([a-z0-9]+)/modify', ['c'=>'Student','a'=>'modify','p'=>['sid']] ],
+    ['GET', '/s/input', ['c'=>'Student','a'=>'input'] ],
+    ['POST', '/s/save', ['c'=>'Student','a'=>'save'] ],
+];
+*/    
     public function __construct($base, $routes)
     {
         $this->base = $base;
